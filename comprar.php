2282d76b2546e33bbc371072527cb4edbc0526ce
Daniel <danivangelis@hotmail.com>
2015-02-18 15:35:17 +0000

    Mudancas para usar gamerefresh

--- comprar.php
+++ comprar.php
@@ -36,30 +36,7 @@
                 echo "alert($pupgrade)";
                 mysqli_query($c, $query);
                 
-                $query = "SELECT * FROM itens, posses WHERE item_id = id_item AND id_user = $userid order by nome";
-                $result = mysqli_query($c, $query);
-                $frase = "";
-                while($row = mysqli_fetch_array($result)){
-                    $id = $row['item_id'];
-                    $nome = $row['nome'];
-                    $imagem = $row['imagem'];
-                    
-                    if(strpos($frase, $nome) === false){
-                        $frase = $frase . "_" . $nome;
-                    }
-                }
-            
-                $frase = substr($frase, 1);
-                $frase = $frase . ".png";
-                
-                if($frase == ".png"){
-                        echo "float: left; margin-top: 17px; margin-left: 150px; height: 420px; width: 600px;" .
-                        "border-radius: 2px; background-image: url(Images/quarto.png); background-size: 100% 100%;";
-                }
-                else{
-                        echo "float: left; margin-top: 17px; margin-left: 150px; height: 420px; width: 600px;" .
-                        "border-radius: 2px; background-image: url(Images/$frase); background-size: 100% 100%;";
-                }
+                include("gamerefresh.php");
         }
         if($num_count == 0){
 
